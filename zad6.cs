f7715e28b3c14f88b9ee3cff79584aad67d915c5
milenkata74 <125346200+milenkata74@users.noreply.github.com>
2025-06-06 10:15:53 +0300

    Add files via upload

---
+++ zad6.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Въведи първо множество:");
+        var set1 = Console.ReadLine().Split().Select(int.Parse).ToHashSet();
+
+        Console.WriteLine("Въведи второ множество:");
+        var set2 = Console.ReadLine().Split().Select(int.Parse).ToHashSet();
+
+        var union = set1.Union(set2);
+        var intersect = set1.Intersect(set2);
+        var diff = set1.Except(set2);
+        var symDiff = union.Except(intersect);
+
+        Console.WriteLine("Сечение: " + string.Join(" ", intersect));
+        Console.WriteLine("Обединение: " + string.Join(" ", union));
+        Console.WriteLine("Разлика: " + string.Join(" ", diff));
+        Console.WriteLine("Допълнение: " + string.Join(" ", symDiff));
+    }
+}
