3133a0fbce3b75b6e37795abd603047f62c8cfbc
milenkata74 <125346200+milenkata74@users.noreply.github.com>
2025-06-06 10:19:04 +0300

    Add files via upload

---
+++ zad8.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random rand = new Random();
+        int[] counts = new int[6];
+        int trials = 100000;
+
+        for (int i = 0; i < trials; i++)
+        {
+            int roll = rand.Next(1, 7); // зар 1 до 6
+            counts[roll - 1]++;
+        }
+
+        for (int i = 0; i < counts.Length; i++)
+        {
+            double probability = (double)counts[i] / trials * 100;
+            Console.WriteLine($"Число {i + 1}: {probability:F2}%");
+        }
+    }
+}
