845d9a8a14ac33f62aa7e77c4a6dc9d21e0a4a83
milenkata74 <125346200+milenkata74@users.noreply.github.com>
2025-06-06 09:49:07 +0300

    Add files via upload

---
+++ zad2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Въведи числа, разделени с интервал: ");
+        var nums = Console.ReadLine().Split().Select(double.Parse).ToList();
+
+        double average = nums.Average();
+        double median = nums.OrderBy(x => x).ElementAt(nums.Count / 2);
+        double mode = nums.GroupBy(x => x).OrderByDescending(g => g.Count()).First().Key;
+
+        Console.WriteLine($"Средно аритметично: {average:F2}");
+        Console.WriteLine($"Медиана: {median}");
+        Console.WriteLine($"Мода: {mode}");
+    }
+}
